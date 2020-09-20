60ec1981f3e39babd0e34d0565f47b558545e03a
Sean Rigby <sean.rigby@azure.lloydsbank.com>
2020-09-20 16:18:18 +0100

    update to repo script

--- make/clone-repos.sh
+++ make/clone-repos.sh
@@ -1,11 +1,12 @@
-#TODO
 #!/usr/bin/env bash	
+#Clones all public repos in a github account to be used within documention.
 
 SOURCEDIR="$1"
 REPOSDIR="$2"
 REPOSPATH="${SOURCEDIR}/${REPOSDIR}"
+GHUSER="cloudoutloud"
     
-REPOS=$() \
+REPOS=$(curl -s https://api.github.com/users/$GHUSER/repos | jq '.[]|.html_url') \
 && rm -fr $REPOSPATH \
 && mkdir -p $REPOSPATH \
 && cd $REPOSPATH && echo "Changed directory" || exit 1
